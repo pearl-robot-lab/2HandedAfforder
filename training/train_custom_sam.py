baddc4cc6463035e3d63e42490cc0c1be929778a
Marvin <marvin.heidigner@stud.tu-darmstadt.de>
2024-09-23 14:50:09 +0000

    include wandb, add accuracy and modify arguments to make it possible to perform inference on one singular image

--- training/train_custom_sam.py
+++ training/train_custom_sam.py
@@ -5,6 +5,7 @@ import monai
 import numpy as np 
 import os
 import torch
+import wandb
 
 from argparse import ArgumentParser
 from PIL import Image
@@ -33,7 +34,7 @@ class SAMDataset(Dataset):
 
     bbox = self.bbox[idx]
     if bbox.shape == (2, 2):
-      prompt = [bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]]
+      prompt = [bbox[0][1], bbox[0][0], bbox[1][1], bbox[1][0]]
     else:
       prompt = bbox.tolist()
 
@@ -50,12 +51,13 @@ class SAMDataset(Dataset):
 
 
 class AffordanceDetection():
-  def __init__(self, model, processor, bbox_eps, batchsize, model_folder, num_epochs, visualization_active=False):
+  def __init__(self, model, processor, bbox_eps, batchsize, model_folder, model_name, num_epochs, visualization_active=False):
     self.model = model
     self.processor = processor
     self.bbox_eps = bbox_eps
     self.batchsize = batchsize
     self.model_folder = model_folder
+    self.model_name = model_name
     self.num_epochs = num_epochs
     self.visualization_active = visualization_active
 
@@ -64,6 +66,8 @@ class AffordanceDetection():
     self.optimizer = Adam(model.mask_decoder.parameters(), lr=1e-5, weight_decay=0)
     self.train_loader = None
     self.validation_loader = None
+
+    
 
   def is_valid(self, aff_left, aff_right):
     assert aff_left.shape == aff_right.shape
@@ -181,6 +185,7 @@ class AffordanceDetection():
 
   def validate_one_epoch(self, epoch_index):
     epoch_vlosses = []
+    accuracies = []
     with torch.no_grad():
       for batch in tqdm(self.validation_loader):
         outputs = self.model(pixel_values=batch["pixel_values"].to(self.device),
@@ -195,15 +200,25 @@ class AffordanceDetection():
                                                     align_corners=False)
         vloss = self.seg_loss(predicted_masks, ground_truth_masks.unsqueeze(1))
         epoch_vlosses.append(vloss.item())
-    return epoch_vlosses
+        acc = monai.metrics.compute_iou(predicted_masks, ground_truth_masks.unsqueeze(1))
+        accuracies.append(acc.item())
+    return epoch_vlosses, accuracies
 
   def train(self, images, affs, bboxes):
 
+    
     dset = SAMDataset(img=images, aff_mask=affs, bbox=bboxes, processor=self.processor)
     validation_split = .2
     shuffle_dataset = True
     random_seed = 42
-
+    wandb.init(
+      project="test",
+      config={
+        "dataset": "Bowl_Dataset (P02_01, P04_02, P14_05, P20_03)",
+        "epochs": self.num_epochs,
+        "batchsize": self.batchsize
+      }
+    )
     dataset_size = len(dset)
     indices =list(range(dataset_size))
     split = int(np.floor(validation_split * dataset_size))
@@ -226,11 +241,15 @@ class AffordanceDetection():
       self.model.train()
       epoch_losses = self.train_one_epoch(epoch)
       self.model.eval()
-      epoch_vlosses = self.validate_one_epoch(epoch)
+      epoch_vlosses, accuracies = self.validate_one_epoch(epoch)
       print(f'EPOCH: {epoch}')
       print(f'Mean loss training: {mean(epoch_losses)}')
       print(f'Mean loss validation: {mean(epoch_vlosses)}')
-    torch.save(self.model.state_dict(), os.path.join(self.model_folder, "bowl_model.pth"))
+      #wandb.log({'train_loss': mean(epoch_losses)})
+      wandb.log({'loss': mean(epoch_vlosses)})
+      wandb.log({'acc': mean(accuracies)})
+    torch.save(self.model.state_dict(), os.path.join(self.model_folder, self.model_name))
+    wandb.finish()
 
   def test_on_single_image(self, img, prompt):
     self.model.to(self.device)
@@ -254,11 +273,14 @@ class AffordanceDetection():
     img_tensor = torch.tensor(img)
     return img_tensor
   
-  def save_result(self, img, mask, out):
+  def save_result(self, img, mask, prompt, out):
     mask = cv2.resize(mask.astype(img.dtype), (img.shape[0], img.shape[1]))
     if max(mask.flatten()) == 1:
       mask = mask * 255
-    Image.fromarray(cv2.addWeighted(mask, 0.5, img[:,:,::-1], 0.5, 0)).save(os.path.join(out))
+    x_min, y_min, x_max, y_max = prompt
+    x_min, y_min, x_max, y_max = int(x_min), int(y_min), int(x_max), int(y_max)
+    overlay = cv2.addWeighted(mask, 0.5, img[:,:,::-1], 0.5, 0)
+    Image.fromarray(cv2.rectangle(overlay, (x_min, y_min),(x_max, y_max), color=(255, 0, 0), thickness=2)).save(os.path.join(out))
 
   def test_on_dataset(self, data_folder, out):
     if not os.path.exists(out):
@@ -276,19 +298,27 @@ class AffordanceDetection():
     for i in range(images.size()[0]):
       img = images[i]
       bbox = bboxes[i]
-      x_min, y_min, x_max, y_max = bbox[0][0], bbox[0][1], bbox[1][0], bbox[1][1]
+      x_min, y_min, x_max, y_max = bbox[0][1], bbox[0][0], bbox[1][1], bbox[1][0]
       prompt = [x_min, y_min, x_max, y_max]
       mask = self.test_on_single_image(img, prompt)
       out_path_pred = os.path.join(out, "predictions", str(i) +  ".jpg")
       out_path_gt = os.path.join(out, "gt", str(i) + ".jpg")
       aff_mask = cv2.cvtColor(affs[i].cpu().detach().numpy().astype(images_arr[i].dtype), cv2.COLOR_GRAY2BGR)
-      self.save_result(images_arr[i], mask, out_path_pred)
-      self.save_result(images_arr[i], aff_mask, out_path_gt)
+      self.save_result(images_arr[i], mask, prompt, out_path_pred)
+      self.save_result(images_arr[i], aff_mask, prompt, out_path_gt)
 
   def load_pretrained_weights(self, checkpoint_file):
     self.model = torch.jit.load(checkpoint_file)
 
-def main(data_folder, bbox_eps, checkpoint_file, visualization_active, batchsize, model_folder, num_epochs, out_images, testing):
+def get_bbox(mask, bbox_eps):
+  y_indices, x_indices, _ = np.where(mask > 0)
+  x_max = max(x_indices) + bbox_eps
+  x_min = min(x_indices) - bbox_eps
+  y_max = max(y_indices) + bbox_eps
+  y_min = min(y_indices) - bbox_eps
+  return [x_min, y_min, x_max, y_max]
+
+def main(data_folder, bbox_eps, checkpoint_file, visualization_active, batchsize, model_folder, model_name, num_epochs, out_images, testing, test_single, img_path, mask_path):
 
   # Download pretrained model
   model = SamModel.from_pretrained("facebook/sam-vit-base")
@@ -299,21 +329,41 @@ def main(data_folder, bbox_eps, checkpoint_file, visualization_active, batchsize
   processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
 
   # Instantiate affordance detection object
-  haff_model = AffordanceDetection(model, processor, bbox_eps, batchsize, model_folder, num_epochs, visualization_active)
+  haff_model = AffordanceDetection(model, processor, bbox_eps, batchsize, model_folder, model_name, num_epochs, visualization_active)
 
-  # Prepare data data
-  aff_all, bbox_arr, images_arr = haff_model.prepare_data(data_folder)
+  if not test_single:
+    # Prepare data data
+    aff_all, bbox_arr, images_arr = haff_model.prepare_data(data_folder)
 
-  # Tensors
-  affs = torch.cat(aff_all)
-  bboxes = torch.tensor(bbox_arr)
-  images = torch.tensor(images_arr)
+    # Tensors
+    affs = torch.cat(aff_all)
+    bboxes = torch.tensor(bbox_arr)
+    images = torch.tensor(images_arr)
 
-  # Training
-  if not testing:
-    haff_model.train(images, affs, bboxes)
+    # Training
+    if not testing:
+      haff_model.train(images, affs, bboxes)
+    else:
+      haff_model.test_on_dataset(data_folder, out_images)
   else:
-    haff_model.test_on_dataset(data_folder, out_images)
+    if not os.path.exists(out_images):
+      os.makedirs(out_images)
+    out = os.path.join(out_images, "out.png")
+    img = cv2.imread(img_path)
+    img_tensor = torch.tensor(img)
+    mask = cv2.imread(mask_path)
+    mask = cv2.resize(mask, (img.shape[1], img.shape[0]))
+    max_shape = max([img.shape[0], img.shape[1]])
+    img = cv2.copyMakeBorder(img, max_shape - img.shape[0], 0, max_shape - img.shape[1], 0, cv2.BORDER_CONSTANT)
+    mask = cv2.copyMakeBorder(mask, max_shape - mask.shape[0], 0, max_shape - mask.shape[1], 0, cv2.BORDER_CONSTANT)
+    prompt = get_bbox(mask, bbox_eps)
+    pred = haff_model.test_on_single_image(img_tensor, prompt)
+    #print("Image Shape: ", img.shape)
+    #print("Mask Shape: ", mask.shape)
+    #print("Pred Shape: ", pred.shape)
+    print(np.where(pred != 0))
+    haff_model.save_result(img, pred, prompt, out)
+
        
 if __name__ == "__main__":
   parser = ArgumentParser()
@@ -321,15 +371,19 @@ if __name__ == "__main__":
   parser.add_argument('--data-folder', default=None, help='directory where the images for training are located')
   parser.add_argument('--visualization-active', default=False, type=bool, help='parameter to indicate whether the data points should be visualized or not')
   parser.add_argument('--model-folder', default=None, help='path to the model folder')
-  parser.add_argument('--batchsize', default=1, type=int)
+  parser.add_argument('--batchsize', default=16, type=int)
   parser.add_argument('--checkpoint', default="")
   parser.add_argument('--out-images', default=None, help='ouput directory')
   parser.add_argument('--num-epochs', default=100, type=int)
   parser.add_argument('--test', default=False, type=bool)
   parser.add_argument('--out', default=None)
+  parser.add_argument('--model-name', default="bowl_model.pth")
+  parser.add_argument('--test-single', default=False, type=bool)
+  parser.add_argument('--img', default=None)
+  parser.add_argument('--prompt', default=None)
   args = parser.parse_args()
   vals = vars(args)
-  if vals["data_folder"] != None and vals["model_folder"] != None:
+  if (vals["test"] and vals["data_folder"] != None and vals["checkpoint"] != None and vals["out"] != None) or (vals["test_single"] and vals["checkpoint"] != None and vals["img"] != None and vals["prompt"] != None and vals["out"] != None) or (vals["data_folder"] != None and vals["model_folder"] != None):
     data_folder = vals["data_folder"]
     bbox_eps = vals["bbox_eps"]
     visualization_active = vals["visualization_active"]
@@ -339,9 +393,10 @@ if __name__ == "__main__":
     num_epochs = vals["num_epochs"]
     testing = vals["test"]
     out_images = vals["out"]
-    if testing and not out_images:
-      print("The output directory was not specified!")
-    else:
-      main(data_folder, bbox_eps, checkpoint_file, visualization_active, batchsize, model_folder, num_epochs, out_images, testing)
+    model_name = vals["model_name"]
+    img_path = vals["img"]
+    mask_path = vals["prompt"]
+    test_single = vals["test_single"]
+    main(data_folder, bbox_eps, checkpoint_file, visualization_active, batchsize, model_folder, model_name, num_epochs, out_images, testing, test_single, img_path, mask_path)
   else:
     print("Wrong Arguments!")
